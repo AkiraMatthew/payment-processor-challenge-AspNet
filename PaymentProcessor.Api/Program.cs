1fb1eb63284b724a2c22df196fdcb5e25716e3d7
Mateus Henrique <akira.dev.math@gmail.com>
2025-07-20 14:24:40 -0300

    feat

--- PaymentProcessor.Api/Program.cs
+++ PaymentProcessor.Api/Program.cs
@@ -1,17 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHttpContextAccessor();
+
+#region Swagger Documentation
+builder.Services.AddSwaggerGen(options =>
+{
+    /*options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description =
+                    "This is a system built for encrypt/decrypt passwords and files, generate passwords, store passwords/sensitive data\r\n\r\n" +
+                    "Type 'Bearer' + your token in the input below, ex.: 'Bearer XYZ'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+    });*/
+    options.SwaggerDoc(apiVersion, new OpenApiInfo
+    {
+        Title = "Payment Processor",
+        Version = apiVersion,
+        Description = "This payment processor works as a proxy that will proccess Brazil's Central Bank financial transactions",
+        Contact = new OpenApiContact
+        {
+            Name = "Mateus Henrique",
+            Email = "akiradigitalsolutionss@gmail.com",
+            Url = new Uri("https://akiradigitalsolutions.com")
+        }
+    });
+});
+#endregion
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseSwaggerUI(options =>
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    options.SwaggerEndpoint($"/swagger/{apiVersion}/swagger.json", "ADS.PassAuthKeeper");
+    options.DocumentTitle = "PassAuthKeeper API Documentation";
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseRouting();
