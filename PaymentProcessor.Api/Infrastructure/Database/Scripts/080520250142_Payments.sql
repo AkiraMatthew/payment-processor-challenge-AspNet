4849161682e931c94af292b21fc21b9192cd7234
Mateus Henrique <akira.dev.math@gmail.com>
2025-08-06 00:32:58 -0300

    feat: payments table creation

--- PaymentProcessor.Api/Infrastructure/Database/Scripts/080520250142_Payments.sql
+++ PaymentProcessor.Api/Infrastructure/Database/Scripts/080520250142_Payments.sql
@@ -1,9 +1,12 @@
+--DROP INDEX IF EXISTS idx_processor_processed;
+--DROP TABLE IF EXISTS payments;
+
 CREATE TABLE payments (
-	id NVARCHAR PRIMARY KEY DEFAULT uuid_generate_v4(),
-	correlation_id NVARCHAR NOT NULL UNIQUE,
-	amount DECIMAL(10,2) NOT NULL,
-	processor_type VARCHAR(10) NOT NULL CHECK (processor_type in ('default', 'fallback')),
-	processed_at TIMESTAMPZ NOT NULL DEFAULT NOW()
+    id VARCHAR(36) PRIMARY KEY,
+    correlation_id VARCHAR(36) NOT NULL UNIQUE,
+    amount DECIMAL(10,2) NOT NULL,
+    processor_type VARCHAR(10) NOT NULL CHECK (processor_type IN ('default', 'fallback')),
+    processed_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX idx_processor_processed ON payments (processor_type, processed_at);
