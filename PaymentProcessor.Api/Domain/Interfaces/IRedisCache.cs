f1ce8c07ca6f6d3bcd41080d0ef34e8eaefd080e
Mateus Henrique <akira.dev.math@gmail.com>
2025-08-08 01:10:58 -0300

    feat: implement Redis Memory Caching

--- PaymentProcessor.Api/Domain/Interfaces/IRedisCache.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PaymentProcessor.Api.Domain.Interfaces;
-
-public interface IRedisCache
-{
-    Task SetAsync<T>(string key, T value, TimeSpan expiry);
-    Task<T?> GetAsync<T>(string key);
-    Task<bool> ExistsAsync(string key);
-    Task<bool> RemoveAsync(string key);
-}
---
+++ PaymentProcessor.Api/Infrastructure/Redis/RedisCacheService.cs
@@ -0,0 +1,75 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Collections.Concurrent;
+
+namespace PaymentProcessor.Api.Infrastructure.Redis;
+
+public class RedisCacheService(IDistributedCache distributedCache) : IRedisCacheService
+{
+    private static readonly ConcurrentDictionary<string, bool> CacheKeys = new();
+
+    public async Task SetAsync<T>(string key, T value, CancellationToken cancellationToken) 
+        where T : class
+    {
+        string cacheValue = JsonConvert.SerializeObject(value);
+
+        await distributedCache.SetStringAsync(key, cacheValue, cancellationToken);
+
+        CacheKeys.TryAdd(key, false);
+    }
+
+    public async Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken) 
+        where T : class
+    {
+        string? cachedValue = await distributedCache.GetStringAsync(
+            key, 
+            cancellationToken);
+
+        if (cachedValue is null)
+            return null;
+
+        return JsonConvert.DeserializeObject<T>(cachedValue);
+    }
+
+    public async Task<T?> GetAsync<T>(string key, Func<Task<T>> factory, CancellationToken cancellationToken = default) where T : class
+    {
+        T? cachedValue = await GetAsync<T>(key, cancellationToken);
+
+        if(cachedValue is not null)
+            return cachedValue;
+
+        cachedValue = await factory();
+
+        await SetAsync(key, cachedValue, cancellationToken);
+
+        return cachedValue;
+    }
+
+    public async Task RemoveAsync(string key, CancellationToken cancellationToken)
+    {
+        await distributedCache.RemoveAsync(key, cancellationToken);
+
+        CacheKeys.TryRemove(key, out bool _);
+    }
+
+    public async Task RemoveByPrefixAsync(string prefixKey, CancellationToken cancellationToken)
+    {
+        //foreach (var key in CacheKeys.Keys)
+        //{
+        //    await RemoveAsync(key, cancellationToken);
+        //}
+
+        IEnumerable<Task> task = CacheKeys
+            .Keys
+            .Where(k => k.StartsWith(prefixKey))
+            .Select(k => RemoveAsync(k, cancellationToken));
+
+        await Task.WhenAll(task);
+    }
+
+    public Task<bool> ExistsAsync(string key)
+    {
+        throw new NotImplementedException();
+    }
+}
