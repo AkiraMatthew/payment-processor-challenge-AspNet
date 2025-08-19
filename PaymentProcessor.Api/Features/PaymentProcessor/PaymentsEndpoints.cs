813e25665d19ff0049edc3d1b0efa2fb27da0265
Mateus Henrique <akira.dev.math@gmail.com>
2025-08-19 00:07:07 -0300

    feat: introduce the initial minimalApi endpoints

---
+++ PaymentProcessor.Api/Features/PaymentProcessor/PaymentsEndpoints.cs
@@ -0,0 +1,10 @@
+﻿namespace PaymentProcessor.Api.Features.PaymentProcessor;
+
+public static class PaymentsEndpoints
+{
+    public static void MapPaymentsApi(this WebApplication app)
+    {
+        app.MapPost("/payments", () => "HelloWorld");
+        app.MapPost("/payments-summary", () => "HelloWorld");
+    }
+}
