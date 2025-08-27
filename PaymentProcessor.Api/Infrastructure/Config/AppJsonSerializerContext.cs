88ae1916c728091fb979a6da144b77c594dab75c
Mateus Henrique <akira.dev.math@gmail.com>
2025-08-27 00:24:45 -0300

    feat: json serializer

---
+++ PaymentProcessor.Api/Infrastructure/Config/AppJsonSerializerContext.cs
@@ -0,0 +1,14 @@
+﻿using PaymentProcessor.Api.Domain.DTOs.GET;
+using PaymentProcessor.Api.Domain.DTOs.POST;
+using PaymentProcessor.Api.Domain.Entities;
+using PaymentProcessor.Api.Infrastructure.Enum;
+using System.Text.Json.Serialization;
+
+namespace PaymentProcessor.Api.Infrastructure.Config;
+
+[JsonSerializable(typeof(Payment))]
+[JsonSerializable(typeof(PaymentRequest))]
+[JsonSerializable(typeof(SummaryResponse))]
+[JsonSerializable(typeof(PaymentGateway))]
+[JsonSerializable(typeof(DateTimeOffset?))]
+public sealed partial class AppJsonSerializerContext : JsonSerializerContext;
