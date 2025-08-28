3227ecf4849968b93e937997b78de881543aa52b
Mateus Henrique <akira.dev.math@gmail.com>
2025-08-27 23:17:30 -0300

    feat: add socket configuration

---
+++ PaymentProcessor.Api/Infrastructure/Config/SocketConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace PaymentProcessor.Api.Infrastructure.Config;
+
+public class SocketConfiguration
+{
+    public const int MaxDegreeOfParallels = 20;
+
+    public static Func<HttpMessageHandler> GetSocketHandler() => () => new SocketsHttpHandler
+    {
+        MaxConnectionsPerServer = int.MaxValue,
+    };
+}
